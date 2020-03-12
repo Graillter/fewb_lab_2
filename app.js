7a6a1f7156f0f54ff613c06dc7a57f5a0195f1e9
Graillter <timotheehongre@gmail.com>
2020-03-12 15:57:05 +0000

    End

---
+++ app.js
@@ -0,0 +1,26 @@
+function myAlphabetLength(myAlphabet)
+{
+    console.log(myAlphabet.length);
+}
+
+let myAlphabet = ['A','B','C','D','E','F','G'];
+
+myAlphabetLength(myAlphabet);
+
+var Planets = ['Jupiter','Saturn','Uranus','Neptune','Pluto'];
+console.log(Planets[0]);
+console.log(Planets[1]);
+console.log(Planets[2]);
+console.log(Planets[3]);
+console.log(Planets[4]);
+
+var wowDatatypes = ['int','char','bool'];
+console.log(wowDatatypes[0]);
+console.log(wowDatatypes[1]);
+console.log(wowDatatypes[2]);
+
+var hello = function()
+{
+    return "Hello world!"
+}
+console.log(hello());
